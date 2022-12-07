4f03fa12fe1883546d20069193eb91a3d0511ff1
Ethan Swan <ethanpswan@gmail.com>
2022-12-06 22:39:35 -0600

    Get minimal prediction api working.

--- api/main.py
+++ api/main.py
@@ -1,8 +1,32 @@
-from fastapi import FastAPI
+import pickle
+from pathlib import Path
 
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import GradientBoostingClassifier
+from fastapi import FastAPI
+from .pydantic_models import Observation, Prediction, FlowerType
+
+MODEL_PATH = Path('./models/mymodel.pickle')
+
+def load_model(path: Path) -> GradientBoostingClassifier:
+    with open(MODEL_PATH, "rb") as f:
+        model = pickle.load(f)
+    return model
+
+model = load_model(MODEL_PATH)
 
 app = FastAPI()
 
 @app.get('/')
 def home():
     return 'the api is up and running!'
+
+@app.post('/prediction')
+def predict(obs: Observation) -> Prediction:
+    output_class_array = model.predict(obs.as_dataframe())
+    # output_class_array is an array, but has only one element -- the prediction for our record.
+    output_class = output_class_array[0]
+    flower_type = FlowerType(output_class)
+    pred = Prediction(flower_type=flower_type)
+    return pred
---
+++ api/pydantic_models.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from pydantic import BaseModel
+from enum import IntEnum
+
+class FlowerType(IntEnum):
+    setosa = 0
+    versicolour = 1
+    virginica = 2
+
+class Observation(BaseModel):
+    # For later parts of this code to work, the order of fields here needs to match the order they were listed in training.
+    sepal_length: float
+    sepal_width: float
+    petal_length: float
+    petal_width: float
+
+    def as_dataframe(self) -> pd.DataFrame:
+        '''Convert this record to a DataFrame with one row.'''
+        # Note: the order here is vital; if these aren't in the same order as they were in training, sklearn will complain.
+        values = (self.sepal_length, self.sepal_width, self.petal_length, self.petal_width)
+        colnames = ('sepal length (cm)', 'sepal width (cm)', 'petal length (cm)', 'petal width (cm)')
+        return pd.DataFrame([values], columns=colnames)
+    
+    def as_series(self) -> pd.Series:
+        values = (self.sepal_length, self.sepal_width, self.petal_length, self.petal_width)
+        colnames = ('sepal length (cm)', 'sepal width (cm)', 'petal length (cm)', 'petal width (cm)')
+        return pd.Series(values, index=colnames)
+
+class Prediction(BaseModel):
+    flower_type: FlowerType
