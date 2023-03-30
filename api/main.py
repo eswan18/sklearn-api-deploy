153fb5ebef6162a2e25d91b08058d8399027a325
Ethan Swan <ethanpswan@gmail.com>
2023-03-29 22:14:28 -0500

    Add batch prediction endpoint.

--- api/main.py
+++ api/main.py
@@ -1,32 +1,50 @@
 import pickle
 from pathlib import Path
 
-import numpy as np
 import pandas as pd
-from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.linear_model import LogisticRegression
 from fastapi import FastAPI
 from .pydantic_models import Observation, Prediction, FlowerType
 
-MODEL_PATH = Path('./models/mymodel.pickle')
+MODEL_PATH = Path("./models/iris_regression.pickle")
 
-def load_model(path: Path) -> GradientBoostingClassifier:
+
+def load_model(path: Path) -> LogisticRegression:
     with open(MODEL_PATH, "rb") as f:
         model = pickle.load(f)
     return model
+
 
 model = load_model(MODEL_PATH)
 
 app = FastAPI()
 
-@app.get('/')
-def home():
-    return 'the api is up and running!'
 
-@app.post('/prediction')
+@app.get("/")
+def home():
+    return "the api is up and running!"
+
+
+@app.post("/predict")
 def predict(obs: Observation) -> Prediction:
+    """Predict the flower type from the given observation."""
     output_class_array = model.predict(obs.as_dataframe())
+    print(output_class_array)
     # output_class_array is an array, but has only one element -- the prediction for our record.
     output_class = output_class_array[0]
     flower_type = FlowerType(output_class)
     pred = Prediction(flower_type=flower_type)
     return pred
+
+
+@app.post("/batch_predict")
+def batch_predict(batch: list[Observation]) -> list[Prediction]:
+    """Predict the flower type for a batch of observations."""
+    rows = [obs.as_row() for obs in batch]
+    df = pd.DataFrame(rows)
+    output_classes = model.predict(df)
+    preds = [
+        Prediction(flower_type=FlowerType(output_class))
+        for output_class in output_classes
+    ]
+    return preds
--- api/pydantic_models.py
+++ api/pydantic_models.py
@@ -2,10 +2,12 @@ import pandas as pd
 from pydantic import BaseModel
 from enum import IntEnum
 
+
 class FlowerType(IntEnum):
     setosa = 0
     versicolour = 1
     virginica = 2
+
 
 class Observation(BaseModel):
     # For later parts of this code to work, the order of fields here needs to match the order they were listed in training.
@@ -15,16 +17,25 @@ class Observation(BaseModel):
     petal_width: float
 
     def as_dataframe(self) -> pd.DataFrame:
-        '''Convert this record to a DataFrame with one row.'''
+        """Convert this record to a DataFrame with one row."""
+        return pd.DataFrame([self.as_row()])
+
+    def as_row(self) -> pd.Series:
         # Note: the order here is vital; if these aren't in the same order as they were in training, sklearn will complain.
-        values = (self.sepal_length, self.sepal_width, self.petal_length, self.petal_width)
-        colnames = ('sepal length (cm)', 'sepal width (cm)', 'petal length (cm)', 'petal width (cm)')
-        return pd.DataFrame([values], columns=colnames)
-    
-    def as_series(self) -> pd.Series:
-        values = (self.sepal_length, self.sepal_width, self.petal_length, self.petal_width)
-        colnames = ('sepal length (cm)', 'sepal width (cm)', 'petal length (cm)', 'petal width (cm)')
+        values = (
+            self.sepal_length,
+            self.sepal_width,
+            self.petal_length,
+            self.petal_width,
+        )
+        colnames = (
+            "sepal length (cm)",
+            "sepal width (cm)",
+            "petal length (cm)",
+            "petal width (cm)",
+        )
         return pd.Series(values, index=colnames)
+
 
 class Prediction(BaseModel):
     flower_type: FlowerType
