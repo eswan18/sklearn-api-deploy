c534597b73de592a25e64671ae4e7380982d3888
Ethan Swan <ethanpswan@gmail.com>
2023-04-04 21:04:18 -0500

    Revamp incremental-diff script.

--- incremental-diff.sh
+++ incremental-diff.sh
@@ -1,1 +1,2 @@
-diff -Nbacr --exclude ".pytest_cache" --exclude "__pycache__" --exclude ".DS_Store" "$1" "$2"
+# May need to install diff2html first -- `npm install -g diff2html-cli`
+diff -Nru --exclude ".pytest_cache" --exclude "__pycache__" --exclude ".DS_Store" "$1" "$2" | diff2html -i stdin -s side
