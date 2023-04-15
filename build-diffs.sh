c60aebf815137fd7493c6d9714c34d8580ae0e54
Ethan Swan <ethanpswan@gmail.com>
2023-04-15 18:17:20 -0500

    Ignore venvs in diffs.

--- build-diffs.sh
+++ build-diffs.sh
@@ -1,8 +1,10 @@
+set -e
+
 # May need to install diff2html first -- `npm install -g diff2html-cli`
 mkdir .empty
-diff -Nru --exclude "*.egg-info" --exclude ".pytest_cache" --exclude "__pycache__" --exclude ".DS_Store" .empty app-section-1 | diff2html -i stdin -s side -F diffs/1.html
-diff -Nru --exclude "*.egg-info" --exclude ".pytest_cache" --exclude "__pycache__" --exclude ".DS_Store" app-section-1 app-section-2 | diff2html -i stdin -s side -F diffs/2.html
-diff -Nru --exclude "*.egg-info" --exclude ".pytest_cache" --exclude "__pycache__" --exclude ".DS_Store" app-section-2 app-section-3 | diff2html -i stdin -s side -F diffs/3.html
-diff -Nru --exclude "*.egg-info" --exclude ".pytest_cache" --exclude "__pycache__" --exclude ".DS_Store" app-section-3 app-section-4 | diff2html -i stdin -s side -F diffs/4.html
+diff -Nru --exclude "venv" --exclude "*.egg-info" --exclude ".pytest_cache" --exclude "__pycache__" --exclude ".DS_Store" .empty app-section-1 | diff2html -i stdin -s side -F diffs/1.html
+diff -Nru --exclude "venv" --exclude "*.egg-info" --exclude ".pytest_cache" --exclude "__pycache__" --exclude ".DS_Store" app-section-1 app-section-2 | diff2html -i stdin -s side -F diffs/2.html
+diff -Nru --exclude "venv" --exclude "*.egg-info" --exclude ".pytest_cache" --exclude "__pycache__" --exclude ".DS_Store" app-section-2 app-section-3 | diff2html -i stdin -s side -F diffs/3.html
+diff -Nru --exclude "venv" --exclude "*.egg-info" --exclude ".pytest_cache" --exclude "__pycache__" --exclude ".DS_Store" app-section-3 app-section-4 | diff2html -i stdin -s side -F diffs/4.html
 
 rmdir .empty
