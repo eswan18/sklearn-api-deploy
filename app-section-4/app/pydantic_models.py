2351c9892d64928bb26aa58877226f976d921081
Ethan Swan <ethanpswan@gmail.com>
2023-04-18 10:43:48 -0500

    Simplify as_row() method.

--- app-section-4/app/pydantic_models.py
+++ app-section-4/app/pydantic_models.py
@@ -18,20 +18,13 @@ class Observation(BaseModel):
         return pd.DataFrame([self.as_row()])
 
     def as_row(self) -> pd.Series:
-        # Note: the order here is vital; if these aren't in the same order as they were in training, sklearn will complain.
-        values = (
-            self.sepal_length,
-            self.sepal_width,
-            self.petal_length,
-            self.petal_width,
-        )
-        colnames = (
-            "sepal length (cm)",
-            "sepal width (cm)",
-            "petal length (cm)",
-            "petal width (cm)",
-        )
-        return pd.Series(values, index=colnames)
+        row = pd.Series({
+            "sepal length (cm)": self.sepal_length,
+            "sepal width (cm)": self.sepal_width,
+            "petal length (cm)": self.petal_length,
+            "petal width (cm)": self.petal_width,
+        })
+        return row
 
 
 class Prediction(BaseModel):
